582823a5d9322c553ea366eca7d99182c8ae8d8e
Michiel Westerbeek <happylinks@gmail.com>
2022-05-01 17:46:30 +0200

    changed config

--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -9,18 +9,18 @@ FLAGS=(
   --enable-nonfree        # required by fdk-aac
   --enable-zlib           # enable zlib
   --enable-libx264        # enable x264
-  --enable-libx265        # enable x265
+  #--enable-libx265        # enable x265
   --enable-libvpx         # enable libvpx / webm
-  --enable-libwavpack     # enable libwavpack
-  --enable-libmp3lame     # enable libmp3lame
+  #--enable-libwavpack     # enable libwavpack
+  #--enable-libmp3lame     # enable libmp3lame
   --enable-libfdk-aac     # enable libfdk-aac
-  --enable-libtheora      # enable libtheora
+  #--enable-libtheora      # enable libtheora
   --enable-libvorbis      # enable libvorbis
-  --enable-libfreetype    # enable freetype
+  #--enable-libfreetype    # enable freetype
   --enable-libopus        # enable opus
-  --enable-libwebp        # enable libwebp
-  --enable-libass         # enable libass
-  --enable-libfribidi     # enable libfribidi
+  #--enable-libwebp        # enable libwebp
+  #--enable-libass         # enable libass
+  #--enable-libfribidi     # enable libfribidi
   # --enable-libaom         # enable libaom
 )
 echo "FFMPEG_CONFIG_FLAGS=${FLAGS[@]}"
